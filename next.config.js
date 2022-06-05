10628e24d56e30f168f1b3bcf9f3d1c31bd05290
Pedro H Domingues <pedroddomingues@gmail.com>
2022-06-05 16:38:14 -0300

    next config ignore ts for now

--- next.config.js
+++ next.config.js
@@ -5,6 +5,13 @@ const nextConfig = {
   images: {
     domains: ['localhost', 'avatars.githubusercontent.com'],
   },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
 }
 
 module.exports = nextConfig
