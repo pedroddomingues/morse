297a60ed9a4975e9818495295eb845b800522e77
Pedro Henrique Domingues <pedroddomingues@gmail.com>
2022-06-09 10:03:32 -0300

    correcting host url in axios

--- src/infra/api/index.ts
+++ src/infra/api/index.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const nextApi = axios.create({
-	baseURL: "http://localhost:3000/api",
+	baseURL: `${process.env.HOST}/api`,
 	headers: { "Content-Type": "application/json" },
 });
