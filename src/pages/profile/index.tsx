3a2f3d6275e363b8cad2b1283eca040cb93bc997
Pedro H Domingues <pedroddomingues@gmail.com>
2022-06-22 22:34:21 -0300

    created new page's base

---
+++ src/pages/profile/index.tsx
@@ -0,0 +1,11 @@
+import LayoutWithHeaderAndFooter from "../../components/LayoutWithHeaderAndFooter";
+
+const Profile = () => {
+	return (
+		<LayoutWithHeaderAndFooter>
+			<h1>Profile</h1>
+		</LayoutWithHeaderAndFooter>
+	);
+};
+
+export default Profile;
---
+++ src/pages/profile/messages/index.tsx
@@ -0,0 +1,11 @@
+import LayoutWithHeaderAndFooter from "../../../components/LayoutWithHeaderAndFooter";
+
+const ProfileMessages = () => {
+	return (
+		<LayoutWithHeaderAndFooter>
+			<h1>ProfileMessages</h1>
+		</LayoutWithHeaderAndFooter>
+	);
+};
+
+export default ProfileMessages;
